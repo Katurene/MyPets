cab7d50e89f84fb9e77fa295b7b676a11e0f7275
Katurene <katurene7@gmail.com>
2021-01-19 23:09:27 +0300

    add index page

--- MyPets/Startup.cs
+++ MyPets/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,28 +13,30 @@ namespace MyPets
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       
         public void ConfigureServices(IServiceCollection services)
         {
+            //добавляем поддержку контролл-в и предст-й mvc
+            services.AddControllersWithViews()
+                //совместимость с asp.net.core 3.0
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();//исп стр исключений для разработчика
             }
 
-            app.UseRouting();
+            app.UseRouting();//маршрутизация
 
+            app.UseStaticFiles();//подкл статич файлов
+
+            //регистрация маршрутов
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
