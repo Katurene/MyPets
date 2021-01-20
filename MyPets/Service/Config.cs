9fe9bc57a051de931fb5d7e1010ef084cc57aa7e
Katurene <katurene7@gmail.com>
2021-01-20 17:14:19 +0300

    add config class and json files

---
+++ MyPets/Service/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPets.Service
+{
+    public class Config    //соотв appsettings.json
+    {
+        public static string ConnectionString { get; set; }
+        public static string CompanyName { get; set; }
+        public static string CompanyPhone { get; set; }
+        public static string CompanyPhoneShort { get; set; }
+        public static string CompanyEmail { get; set; }
+    }
+}
--- MyPets/Startup.cs
+++ MyPets/Startup.cs
@@ -6,16 +6,22 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyPets.Service;
 
 namespace MyPets
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
        
         public void ConfigureServices(IServiceCollection services)
         {
+            //подкл конфиг из appsettings.json("Project"=стр. 2) и new Config()-класс
+            Configuration.Bind("Project", new Config());
             //добавляем поддержку контролл-в и предст-й mvc
             services.AddControllersWithViews()
                 //совместимость с asp.net.core 3.0
