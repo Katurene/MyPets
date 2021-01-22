313116446685584d77ba06aefd54f56b261db035
Katurene <katurene7@gmail.com>
2021-01-22 15:58:09 +0300

    add admin index page

--- MyPets/Areas/Admin/Controllers/HomeController.cs
+++ MyPets/Areas/Admin/Controllers/HomeController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyPets.Domain;
 
 namespace MyPets.Areas.Admin.Controllers
 {
     [Area("Admin")]//пометить область Admin чтобы правило вступило в силу 
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;//для доступа к доменной модели/объектам
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.ServiceItems.GetServiceItems());//вывод списка всех услуг
         }
     }
 }
