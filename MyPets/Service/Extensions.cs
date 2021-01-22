cfad8c605d737507040415f5dcbddca2d6353a00
Katurene <katurene7@gmail.com>
2021-01-22 20:09:40 +0300

    add ckeditor for admin

---
+++ MyPets/Service/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPets.Service
+{
+    public static class Extensions
+    {
+        public static string CutController(this string str)//метод расширения для строки
+        {
+            return str.Replace("Controller", "");
+        }
+    }
+}
