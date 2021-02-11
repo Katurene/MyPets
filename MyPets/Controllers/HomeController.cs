01a89ec0ee788747ff9fa309f8d5a70f7c10694a
Katurene <katurene7@gmail.com>
2021-02-11 23:46:52 +0300

    Add Contacts

--- MyPets/Controllers/HomeController.cs
+++ MyPets/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyPets.Domain;
 
 namespace MyPets.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;//для доступа к бд
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
